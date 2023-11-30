292bc1ec10b55bee165b4114e4e5390caf554004
Shraddha_Sh <shraddha.sharma.ds.2021@mitmeerut.ac.in>
2023-12-01 04:48:18 +0530

    adding main file

---
+++ my_app.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+         # Stock Price
+         
+         shown are the stock closing price and volume of google!
+         """)
